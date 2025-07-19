3daa86742a12faf4d4711821828e9b8962606b29
Abhishek <abhishek.dx23@acropolis.in>
2025-07-19 22:20:57 +0530

    Changes in scroll bar and projects

--- src/main/java/in/abhishekMishra/portfolio/controller/HomeController.java
+++ src/main/java/in/abhishekMishra/portfolio/controller/HomeController.java
@@ -12,16 +12,16 @@ public class HomeController {
     @GetMapping({ "/", "/home" })
     public String showHomePage(Model model) {
         // ✅ Move the tag list creation to Java code
-        List<String> ecommerceTags = List.of("Spring Boot", "MySQL", "REST API", "Thymeleaf");
-        model.addAttribute("ecommerceTags", ecommerceTags);
+        List<String> expenseTrackerTags = List.of("Spring Boot", "MySQL", "REST API", "React Native", "Docker", "Kafka", "Kubernetes");
+        model.addAttribute("expenseTrackerTags", expenseTrackerTags);
 
-        List<String> portfolioTags = List.of("Java", "Spring Boot", "Thymeleaf", "HTML", "CSS");
+        List<String> portfolioTags = List.of("Java", "Spring Boot", "Thymeleaf", "HTML", "CSS", "Java Script", "Maven", "Docker");
         model.addAttribute("portfolioTags", portfolioTags);
 
         List<String> blogTags = List.of("Java", "Spring Boot", "Thymeleaf", "HTML", "CSS");
         model.addAttribute("blogTags", blogTags);
 
-        List<String> ticketBookingTags = List.of("Java", "Maven", "Mapping", "Hash PPassword");
+        List<String> ticketBookingTags = List.of("Java", "Maven", "Mapping", "Hash Password", "BCrypt");
         model.addAttribute("ticketBookingTags", ticketBookingTags);
         
         return "home"; // This renders home.html which includes project fragment
