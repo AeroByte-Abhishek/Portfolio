1fc61ebd31b1f2978da8a24780be3be9f337810e
Abhishek <abhishek.dx23@acropolis.in>
2025-07-21 21:36:46 +0530

    Created popup for empty field

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -31,13 +31,36 @@ window.addEventListener("scroll", () => {
   }
 });
 
-document.querySelector(".contact-form").addEventListener("submit", function(e){
-  const name = document.querySelector("#name").value.trim();
-  const email = document.querySelector("#email").value.trim();
-  const message = document.querySelector("#message").value.trim();
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".contact-form").addEventListener("submit", function (e) {
+    const name = document.querySelector("#name").value.trim();
+    const email = document.querySelector("#email").value.trim();
+    const message = document.querySelector("#message").value.trim();
 
-  if( !name || !email || !message){
-    e.preventDefault();
-    alert("Please fill in all fields before submitting.");
-  }
+    if (!name || !email || !message) {
+      e.preventDefault();
+      showPopup("Missing Fields", " Please fill in all fields. All are mandatory. Your message is important to us, and we’d love to hear from you!");
+    }
+  });
 });
+
+
+// Popup modal
+function showPopup(title, message){
+  const popup = document.querySelector("#popup-card");
+  document.querySelector("#popup-title").textContent = title;
+  document.querySelector("#popup-message").textContent = message;
+
+  popup.classList.remove("hidden");
+  popup.classList.add("visible");
+
+  setTimeout(() => {
+    closePopup();
+  }, 5000);
+}
+
+function closePopup(){
+  const popup = document.querySelector("#popup-card");
+  popup.classList.remove("visible");
+  popup.classList.add("hidden");
+}
